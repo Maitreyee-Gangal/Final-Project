95e6d2eb62c89431d16c360d99e57dde221eadf8
Maitreyee-Gangal <maitreyeegangal2059@gmail.com>
2023-08-15 14:01:53 -0400

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Maitreyee Gangal on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
