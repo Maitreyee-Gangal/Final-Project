7057435862e253cd784b2b74b5d51be4603c2804
Zamira <zamira.gab07@icloud.com>
2023-08-16 10:45:28 -0400

    New Change

---
+++ Final Project/ForgotPasswordController.swift
@@ -0,0 +1,20 @@
+//
+//  ForgotPasswordController.swift
+//  Final Project
+//
+//  Created by Zamira Gabdyzhamalova on 2023-08-16.
+//
+
+import SwiftUI
+
+struct ForgotPasswordController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ForgotPasswordController_Previews: PreviewProvider {
+    static var previews: some View {
+        ForgotPasswordController()
+    }
+}
